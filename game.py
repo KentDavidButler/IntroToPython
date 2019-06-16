c7f2ea44dee85d2da761ea848361ff7bc01577c0
Kent Butler <KentDavidButler@gmail.com>
2019-06-15 20:54:19 -0400

    Images and testing basic movement of object on screen

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 #Basic game setup and set up the display
 #we will create some functionality that pygame already has
 import pygame
-
+import sys, os
 
 #initial pygame
 pygame.init()
@@ -21,6 +21,15 @@ game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 game_screen.fill(WHITE_COLOR)
 pygame.display.set_caption(SCREEN_TITLE)
 
+player_image = pygame.image.load('player.png')
+#casting an image on an image and scaling it to 50
+player_image = pygame.transform.scale(player_image, (50,50))
+rect_loc = 350
+cir_loc = 300
+speed = 5
+rect_right = True
+cir_right = False
+
 #game loop: something that contains all of the game logic
 while not is_game_over:
     
@@ -29,6 +38,21 @@ while not is_game_over:
             is_game_over = True
 
         print(event)
+    #move rect right
+    if rect_right:
+        rect_loc += speed
+    else:
+        rect_loc -= speed
+    if rect_loc >= 700:
+        rect_right = False
+    if rect_loc <= 0:
+        rect_right = True
+        
+#drawing basic shapes: polygon, ellipse, arc, line, lines, aaline & aalines
+    pygame.draw.rect(game_screen, BLACK_COLOR, [350, 350, 100, 100])
+    pygame.draw.circle(game_screen, BLACK_COLOR, (400,300), 50)
+
+    game_screen.blit(player_image, (rect_loc, 375))
 
     #needed logic to update the screen and graphics
     pygame.display.update()
